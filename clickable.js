d4114f915700a0c37f58228a6a95dc4f66e39703
Joshua An <joshuaan7@gmail.com>
2021-01-11 23:50:57 -0800

    compressiblility

--- clickable.js
+++ clickable.js
@@ -1,12 +1,21 @@
 function ShowAndHide(i) {
-    var x = document.getElementById(i);
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
+  var x = document.getElementById(i);
+  var y = document.getElementById("exp");
+  var content = document.getElementById("be-content");
 
-function klikaj(i)
-{ document.getElementById(i).style.visibility='visible';
+  if (x.style.display == 'none') {
+    x.style.display = 'block';
+    x.scrollIntoView();
+    //content.style.maxHeight = content.scrollHeight + "px";
+    document.getElementById('nucsanucsa').style.marginTop = content.scrollHeight + "px";
+    //document.getElementById('nucsanucsa').style.marginTop = '15%';
+
+
+  } else {
+    x.style.display = 'none';
+    y.scrollIntoView();
+    document.getElementById('nucsanucsa').style.marginTop = "-" + content.scrollHeight + "px";
+    //document.getElementById('nucsanucsa').style.marginTop = '-15%';
+
+  }
 }
