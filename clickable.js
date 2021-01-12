517f5f568bc59fb9d5d517182ee3c43cba7952f2
Joshua An <joshuaan7@gmail.com>
2021-01-12 14:27:20 -0800

    looks okay

--- clickable.js
+++ clickable.js
@@ -1,20 +1,20 @@
-function ShowAndHide(i) {
+function ShowAndHide(i, header, row) {
   var x = document.getElementById(i);
-  var y = document.getElementById("exp");
+  var y = document.getElementById(header);
   var content = document.getElementById("be-content");
 
   if (x.style.display == 'none') {
     x.style.display = 'block';
     x.scrollIntoView();
     //content.style.maxHeight = content.scrollHeight + "px";
-    document.getElementById('nucsanucsa').style.marginTop = content.scrollHeight + "px";
+    document.getElementById(row).style.marginTop = (content.scrollHeight+10) + "px";
     //document.getElementById('nucsanucsa').style.marginTop = '15%';
 
 
   } else {
     x.style.display = 'none';
     y.scrollIntoView();
-    document.getElementById('nucsanucsa').style.marginTop = "-" + content.scrollHeight + "px";
+    document.getElementById(row).style.marginTop = "-" + (content.scrollHeight+10) + "px";
     //document.getElementById('nucsanucsa').style.marginTop = '-15%';
 
   }
