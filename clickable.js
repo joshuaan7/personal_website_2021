1cc3113a86400ee61797888bf1d8ddda1cd8cc2d
Joshua An <joshuaan7@gmail.com>
2021-01-13 19:46:40 -0800

    Functionailty of wwebsite working

--- clickable.js
+++ clickable.js
@@ -1,21 +1,38 @@
-function ShowAndHide(i, header, row) {
-  var x = document.getElementById(i);
+function ShowAndHide(card, card_content, header, row) {
+  var x = document.getElementById(card);
   var y = document.getElementById(header);
-  var content = document.getElementById("be-content");
+  var content = document.getElementById(card_content);
 
-  if (x.style.display == 'none') {
+  var all_cards = document.getElementsByClassName("child-div");
+  if (x.style.display == 'none')
+  {
+    //Loop through all cards and see if any are active, if they are, close them
+    //row is parent parent next. parentNode.parentNode.nextElementSibling.id
+    for (i = 0; i < all_cards.length; i++)
+    {
+      if (all_cards[i].style.display == 'block')
+      {
+        all_cards[i].style.display = 'none';
+        const parent_parent_next_id = all_cards[i].parentNode.parentNode.nextElementSibling.id;
+        var child_nodes = all_cards[i].childNodes;
+
+        var child_content = child_nodes[1];
+        //document.getElementById(row).style.marginTop = "-" + (content.scrollHeight) + "px";
+        document.getElementById(parent_parent_next_id).style.marginTop = "-" + (child_content.scrollHeight) + "px";
+
+      }
+    }
+
     x.style.display = 'block';
     x.scrollIntoView();
-    //content.style.maxHeight = content.scrollHeight + "px";
-    document.getElementById(row).style.marginTop = (content.scrollHeight+10) + "px";
-    //document.getElementById('nucsanucsa').style.marginTop = '15%';
+    document.getElementById(row).style.marginTop = (content.scrollHeight) + "px";
+  }
 
-
-  } else {
+  else
+  {
     x.style.display = 'none';
     y.scrollIntoView();
-    document.getElementById(row).style.marginTop = "-" + (content.scrollHeight+10) + "px";
-    //document.getElementById('nucsanucsa').style.marginTop = '-15%';
+    document.getElementById(row).style.marginTop = "-" + (content.scrollHeight) + "px";
 
   }
 }
